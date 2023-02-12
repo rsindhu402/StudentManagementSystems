8bd789359865daff44d7c8f4e089924b9b2489b8
rsindhu402 <125201062+rsindhu402@users.noreply.github.com>
2023-02-12 20:57:08 +0530

    Add files via upload

---
+++ StudentManagementProject/StudentApp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from StudentApp import views
+
+urlpatterns = [
+    path('',views.log_fun,name='log'), # it will display login.css page
+    path('logdata',views.logdata_fun), # it will read data and verify as superuser
+                                       # and redirect to home.html page
+    path('reg',views.reg_fun,name='reg'), #it will redirect to register.html page
+    path('regdata',views.regdata_fun), # it will create a superuser account
+
+    path('home',views.home_fun,name='home'), # it will redirect to home.html
+
+    path('add_students',views.addstudent_fun,name='add'), # it will redirect to addstudent.html
+    path('readdata',views.readdata_fun), # it will insert record into student table
+
+    path('display',views.display_fun,name='display'), # it will display student table data in display.html file
+    path('update/<int:id>',views.update_fun,name='up'), # it will update the student data
+    path('delete/<int:id>',views.delete_fun,name='del'), # it will delete the record from student table
+
+    path('log_out',views.log_out_fun,name='log_out'),
+]
